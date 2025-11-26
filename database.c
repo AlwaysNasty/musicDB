dcf0b7dc2e3748cf2d39cea4357d6ef27149c4cd
AlwaysNasty <angrykd00@gmail.com>
2025-11-26 23:13:24 +0300

    added edit_song function

--- database.c
+++ database.c
@@ -1,6 +1,7 @@
 #include "database.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void init_database(MusicDatabase* db) {
     db->capacity = 10;
@@ -38,3 +39,67 @@ void delete_song(MusicDatabase* db, const unsigned int id) {
     }
     db->count--;
 }
+
+void edit_song(const MusicDatabase* db, const unsigned int id) {
+    if (id == 0 || id > db->count) {
+        printf("Error: Wrong id.\n");
+        return;
+    }
+
+    Song_t* s = &db->songs[id - 1];
+
+    printf("\nРедактирование песни (ID %u)\n", s->id);
+    printf("1: Название:      %s\n", s->title);
+    printf("2: Автор:         %s\n", s->author);
+    printf("3: Жанр:          %s\n", s->genre);
+    printf("4: Год:           %u\n", s->year);
+    printf("5: Длительность:  %u сек\n", s->duration_sec);
+    printf("0: Отмена\n");
+
+    printf("Выберите поле для изменения: ");
+    int choice;
+    scanf("%d", &choice);
+    getchar();
+
+    void input_string(char* buf, size_t size);
+
+    switch (choice) {
+        case 1:
+            printf("Новое название: ");
+            input_string(s->title, sizeof(s->title));
+            break;
+        case 2:
+            printf("Новый автор: ");
+            input_string(s->author, sizeof(s->author));
+            break;
+        case 3:
+            printf("Новый жанр: ");
+            input_string(s->genre, sizeof(s->genre));
+            break;
+        case 4:
+            printf("Новый год: ");
+            scanf("%u", &s->year);
+            getchar();
+            break;
+        case 5:
+            printf("Новая длительность: ");
+            scanf("%u", &s->duration_sec);
+            getchar();
+            break;
+        case 0:
+            printf("Отмена!\n");
+            break;
+        default:
+            printf("Неверный выбор!\n");
+    }
+    printf("Изменения сохранены.\n");
+}
+
+void input_string(char* buf, const size_t size) {
+    fgets(buf, size, stdin);
+    buf[strcspn(buf, "\n")] = 0;
+}
+
+
+
+
